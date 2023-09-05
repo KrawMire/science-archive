4ff3bf19837cb1595c17f85e7f2c663b6529483f
Антон Гаврилов <gavor0120@gmail.com>
2023-09-05 23:48:37 +0400

    admin page components

---
+++ science-archive-web/src/app/modules/account/pages/my-articles-page/my-articles-page.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-my-articles-page',
+  templateUrl: './my-articles-page.component.html',
+  styleUrls: ['./my-articles-page.component.scss']
+})
+export class MyArticlesPageComponent {
+
+}
