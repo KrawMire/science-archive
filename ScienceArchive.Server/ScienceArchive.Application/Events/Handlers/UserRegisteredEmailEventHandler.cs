5589355d8d6bfa30109751387eeb3e46d3ad836a
Anton Gavrilov <60151375+KrawMire@users.noreply.github.com>
2024-05-13 00:34:58 +0200

    added template service. implemented resending confirmation code (#26)

--- ScienceArchive.Server/ScienceArchive.Application/Events/Handlers/UserRegisteredEmailEventHandler.cs
+++ ScienceArchive.Server/ScienceArchive.Application/Events/Handlers/UserRegisteredEmailEventHandler.cs
@@ -1,5 +1,5 @@
-using System.Reflection;
 using ScienceArchive.Application.Abstractions.Persistence;
+using ScienceArchive.Application.Abstractions.Templating;
 using ScienceArchive.Application.Events.EventWrappers;
 using ScienceArchive.Application.Interfaces;
 using ScienceArchive.Core.Domain.Aggregates.Notification;
@@ -13,41 +13,28 @@ internal class UserRegisteredEmailEventHandler : IEventHandler<UserRegisteredEve
 {
     private readonly IDbUnitOfWork _dbUnitOfWork;
     private readonly INotificationGateway _notificationGateway;
+    private readonly ITemplateService _templateService;
     
     public UserRegisteredEmailEventHandler(
         IDbUnitOfWork dbUnitOfWork, 
-        INotificationGateway notificationGateway)
+        INotificationGateway notificationGateway, 
+        ITemplateService templateService)
     {
         _dbUnitOfWork = dbUnitOfWork;
         _notificationGateway = notificationGateway;
+        _templateService = templateService;
     }
 
     public async Task Handle(UserRegisteredEventWrapper eventWrapper, CancellationToken cancellationToken)
     {
-        var assemblyPath = Assembly.GetExecutingAssembly().Location;
-        var assemblyFolder = Path.GetDirectoryName(assemblyPath)!;
-        var htmlTemplateFilePath = Path.Combine(assemblyFolder, "Resources", "EmailTemplates", "otp-code.template.html");
-        
-        if (!File.Exists(htmlTemplateFilePath))
-        {
-            throw new FileNotFoundException("Cannot find template file for email");
-        }
-        
-        var emailTemplate = await File.ReadAllTextAsync(htmlTemplateFilePath, cancellationToken);
+        var populatedEmail = await _templateService.GetPopulatedOtpEmailTemplate(eventWrapper.Event.Name, eventWrapper.Event.ConfirmationCode);
         
         var notification = new Notification(NotificationId.CreateNew())
         {
-            Message = PopulateTemplate(emailTemplate, eventWrapper.Event),
+            Message = populatedEmail,
             Receiver = eventWrapper.Event.Email
         };
 
         await _notificationGateway.SendNotification(notification);
-    }
-
-    private string PopulateTemplate(string template, UserRegisteredEvent domainEvent)
-    {
-        return template
-            .Replace("{UserName}", domainEvent.Name)
-            .Replace("{ConfirmCode}", domainEvent.ConfirmationCode);
     }
 }
--- ScienceArchive.Server/ScienceArchive.Application/UseCases/AuthUseCases/ResendConfirmationCodeUseCase.cs
+++ ScienceArchive.Server/ScienceArchive.Application/UseCases/AuthUseCases/ResendConfirmationCodeUseCase.cs
@@ -1,6 +1,9 @@
+using ScienceArchive.Application.Abstractions.Templating;
 using ScienceArchive.Application.Dtos.Auth.Request;
 using ScienceArchive.Application.Dtos.Auth.Response;
 using ScienceArchive.Application.Interfaces;
+using ScienceArchive.Core.Domain.Aggregates.Notification;
+using ScienceArchive.Core.Domain.Aggregates.Notification.ValueObjects;
 using ScienceArchive.Core.Domain.Aggregates.User.ValueObjects;
 using ScienceArchive.Core.Domain.Services;
 using ScienceArchive.Core.Gateways;
@@ -9,19 +12,38 @@ namespace ScienceArchive.Application.UseCases.AuthUseCases;
 
 internal class ResendConfirmationCodeUseCase : IUseCase<ResendConfirmationCodeRequestDto, ResendConfirmationCodeResponseDto>
 {
+    private readonly ITemplateService _templateService;
     private readonly INotificationGateway _notificationGateway;
     private readonly IAuthService _authService;
 
-    public ResendConfirmationCodeUseCase(INotificationGateway notificationGateway, IAuthService authService)
+    public ResendConfirmationCodeUseCase(
+        INotificationGateway notificationGateway, 
+        IAuthService authService, 
+        ITemplateService templateService)
     {
         _notificationGateway = notificationGateway;
         _authService = authService;
+        _templateService = templateService;
     }
 
     public async Task<ResendConfirmationCodeResponseDto> Handle(ResendConfirmationCodeRequestDto request, CancellationToken cancellationToken)
     {
         var (user, code) = await _authService.RegenerateConfirmCode(UserId.CreateFromString(request.UserId));
-        // await _notificationGateway.SendNotification(contract.UserId, code);
+
+        if (code is null)
+        {
+            return new ResendConfirmationCodeResponseDto(user.Id.ToString());
+        }
+        
+        var populatedEmail = await _templateService.GetPopulatedOtpEmailTemplate(user.Name, code);
+        
+        var notification = new Notification(NotificationId.CreateNew())
+        {
+            Message = populatedEmail,
+            Receiver = user.Email
+        };
+        await _notificationGateway.SendNotification(notification);
+        
         return new ResendConfirmationCodeResponseDto(user.Id.ToString());
     }
 }
